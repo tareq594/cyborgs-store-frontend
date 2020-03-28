783f4d3b225aeda7d07be483663a0300ff33c091
tareqsanabra <tareq.sanabra@gmail.com>
2020-03-28 13:39:26 +0100

    de

--- config/store.js
+++ config/store.js
@@ -4,5 +4,5 @@ module.exports = {
 	language: process.env.LANGUAGE || 'de',
 	// used by Store (server side)
 	ajaxBaseUrl: process.env.AJAX_BASE_URL || 'https://api.demo.store.cyborgstech.com/ajax',
-	developerMode: process.env.DEVELOPER_MODE || true
+	developerMode: process.env.DEVELOPER_MODE || false
 };
