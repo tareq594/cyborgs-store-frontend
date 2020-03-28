3361056d0691825038d36476f8811e845040bbd6
tareqsanabra <tareq.sanabra@gmail.com>
2020-03-28 13:32:58 +0100

    de

--- config/store.js
+++ config/store.js
@@ -1,7 +1,7 @@
 // config used by store client side only
 module.exports = {
 	// store UI language
-	language: process.env.LANGUAGE || 'en',
+	language: process.env.LANGUAGE || 'de',
 	// used by Store (server side)
 	ajaxBaseUrl: process.env.AJAX_BASE_URL || 'https://api.demo.store.cyborgstech.com/ajax',
 	developerMode: process.env.DEVELOPER_MODE || true
